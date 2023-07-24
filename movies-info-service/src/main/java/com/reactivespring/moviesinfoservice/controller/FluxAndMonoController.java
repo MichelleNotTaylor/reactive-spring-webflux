30546fa5133f34a0e8cac918b8870976959d865d
Michelle Taylor <michelle.taylor@parchment.com>
2023-07-24 09:44:30 -0500

    Controller

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/FluxAndMonoController.java
@@ -1,8 +1,12 @@
 package com.reactivespring.moviesinfoservice.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class FluxAndMonoController {
@@ -11,4 +15,15 @@ public class FluxAndMonoController {
     public Flux<Integer> flux() {
         return Flux.just(1,2,3);
     }
+
+    @GetMapping("/mono")
+    public Mono<String> helloWorldMono() {
+        return Mono.just("Hello, World!");
+    }
+
+    @GetMapping(value="/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
+    }
+
 }
---
+++ movies-info-service/src/test/java/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,55 @@
+package com.reactivespring.moviesinfoservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebFluxTest(FluxAndMonoController.class)
+@AutoConfigureWebTestClient
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void flux() {
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void helloWorldMono() {
+        webTestClient
+                .get()
+                .uri("/mono")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .consumeWith(stringEntityExchangeResult -> {
+                    var responseBody = stringEntityExchangeResult.getResponseBody();
+                    assertEquals("Hello, World!", responseBody);
+                });
+    }
+
+    @Test
+    void stream() {
+        webTestClient
+                .get()
+                .uri("/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+}
